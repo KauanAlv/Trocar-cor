50c99845dc14aeaf97de124c1ef59a4c454a3007
Kauan Alves <kauan0alvess@gmail.com>
2026-02-03 10:51:12 -0300

    Alteração no estilo do cursor

--- main.js
+++ main.js
@@ -6,13 +6,13 @@ const botaoVermelho = document.getElementById('botao-vermelho')
     
 function trocarCor(){
     const corUsuario = document.getElementById('cor-usuario').value
-    if(corUsuario == 'azul'){
+    if(corUsuario == 'azul' || corUsuario == 'Azul'){
         document.documentElement.style.setProperty('--color-bg', '#008eff')
-    }else if(corUsuario == 'bronze'){
+    }else if(corUsuario == 'bronze' || corUsuario == 'Bronze'){
         document.documentElement.style.setProperty('--color-bg', '#CD7F32')
-    }else if(corUsuario == 'prata'){
+    }else if(corUsuario == 'prata' || corUsuario == 'Prata'){
         document.documentElement.style.setProperty('--color-bg', '#BAB8B5')
-    }else if(corUsuario == 'ouro'){
+    }else if(corUsuario == 'ouro' || corUsuario == 'Ouro'){
         document.documentElement.style.setProperty('--color-bg', '#D3AF37')
     }else{
         document.documentElement.style.setProperty('--color-bg', corUsuario)
